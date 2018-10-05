c46936b62ff2e5691fdae3ed8ccd1ba8577c26c1
nomnel <laxyz.nomnel@gmail.com>
2018-10-05 17:10:06 +0900

    :sparkles: create Page model

---
+++ db/migrate/20181005080105_create_pages.rb
@@ -0,0 +1,10 @@
+class CreatePages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pages do |t|
+      t.text :url, null: false
+
+      t.timestamps
+    end
+    add_index :pages, :url, unique: true
+  end
+end
