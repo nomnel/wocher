462ba5446f759e7a89659a2032be0bc9184895b6
nomnel <laxyz.nomnel@gmail.com>
2018-10-06 22:34:45 +0900

    :sparkles: fetch and save page data

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,4 +5,13 @@ class Page < ApplicationRecord
 
   validates :title, presence: true
   validates :url, presence: true, uniqueness: true
+
+  def self.pull!
+    HatenaBlog::Api.new.entries.each do |entry|
+      page = find_or_initialize_by(url: entry.url)
+      page.title = entry.title
+      break unless page.changed?
+      page.save!
+    end
+  end
 end
