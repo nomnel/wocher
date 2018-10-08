b961bc239dcaca57a6c22c26c5d76bdcf018ed38
nomnel <laxyz.nomnel@gmail.com>
2018-10-08 13:21:43 +0900

    :sparkles: get most hatena bookmark count increased items

--- app/models/hatena_bookmark/snapshot.rb
+++ app/models/hatena_bookmark/snapshot.rb
@@ -5,4 +5,16 @@ class HatenaBookmark::Snapshot < ApplicationRecord
     numericality: { greater_than_or_equal_to: 0, only_integer: true },
     presence: true
   validates :date, presence: true
+
+  def self.most_increased(limit: 3, from_date:)
+    date = maximum(:date)
+    join_clause = sanitize_sql(
+      ["left outer join #{table_name} as b on a.page_id = b.page_id and b.date = ?", from_date])
+    select('a.*, (a.count - coalesce(b.count, 0)) as increased_count').
+      from("#{table_name} as a").
+      joins(join_clause).
+      where('a.date = ?', date).
+      order('increased_count desc').
+      limit(limit)
+  end
 end
